084e573ad169426e104ada7a88418030df6300fa
Zhang Kang <kang.zhang@emc.com>
2018-08-15 17:42:37 +0800

    Pointer and Non Pointer call different function

--- elem.go
+++ elem.go
@@ -1,3 +1,7 @@
+/*
+This package tell us that point and struct will call different function
+if they have the same function sign except pass by value or pointer
+*/
 package main
 
 import (
@@ -6,27 +10,29 @@ import (
 
 type Orange struct {
 	a int
-	c chan int
 }
 
-func(o *Orange)Incream(num int){
+func (o *Orange) Incream(num int) {
 	o.a += num
 }
 
-func(o *Orange)Decream(num int){
+func (o *Orange) Decream(num int) {
 	o.a -= num
 }
 
-func(o *Orange)String() string {
+func (o *Orange) String() string {
 	fmt.Println("1")
-	return string(o.a)
+	return fmt.Sprintf("int(%d)", o.a)
 }
 
 func main() {
+	// in go, does not exist uninitialized variable. This is nil in struct for each struct element
 	var o Orange
 	o.Incream(10)
 	o.Decream(5)
+	fmt.Println(o)
+	// here call func(o Orange)String() string
 	fmt.Printf("%v", o)
-  
-  // {5 <nil>}
+
+	// {5 <nil>}
 }
